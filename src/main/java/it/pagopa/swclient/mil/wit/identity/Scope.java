18e1af09e54df18a66e44079e851876cc3139427
antoniotarricone <antonio.tarricone@pagopa.it>
2024-08-02 17:39:08 +0200

    Draft!

--- src/main/java/it/pagopa/swclient/mil/wit/identity/Scope.java
+++ src/main/java/it/pagopa/swclient/mil/wit/identity/Scope.java
@@ -18,14 +18,14 @@ public class Scope {
 	 * Scope to get an access token to access to Key Vault APIs.
 	 * </p>
 	 */
-	public static final String VAULT = "https://vault.azure.net";
+	public static final String VAULT = "https://vault.azure.net/.default";
 
 	/**
 	 * <p>
 	 * Scope to get an access token to access to Storage Account APIs.
 	 * </p>
 	 */
-	public static final String STORAGE = "https://storage.azure.com";
+	public static final String STORAGE = "https://storage.azure.com/.default";
 
 	/**
 	 * <p>
